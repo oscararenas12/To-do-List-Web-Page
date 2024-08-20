45809f594056f206982e633cc582c1323c14405a
oscararenas12 <oscararenas625@pm.me>
2024-08-20 11:12:26 -0700

    Updating coding, ended on an error here but need to commit

--- lists/tests.py
+++ lists/tests.py
@@ -4,10 +4,9 @@ class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertContains(response, "A new list item")
+        self.assertTemplateUsed(response, "home.html")
